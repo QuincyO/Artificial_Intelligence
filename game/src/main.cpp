874bd129764d85c2c6549085da3c3e86738bc496
QuincyO <quincyorta@gmail.com>
2023-05-13 04:28:03 -0400

    Shapes On Screen

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,5 +1,5 @@
 #include "rlImGui.h"
-#define SCREEN_WIDTH 576
+#define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 800
 #include <iostream>
 
@@ -12,119 +12,173 @@ using namespace std;
 Vector2 centerScreen = { SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2 };
 Vector2 mousePOS;
 
+
+class currencyButton
+{
+public:
+    currencyButton(int coinValue, Rectangle rect,float radius,int shape)
+    {
+        this->value = coinValue;
+        this->rect.x = rect.x;
+        this->rect.y = rect.y;
+        this->rect.width = rect.width;
+        this->rect.height = rect.height;
+        this->position.x = rect.x;
+        this->position.y = rect.y;
+        this->radius = radius;
+        rotation = 0;
+        this->shape = shape;
+    };
+
+    int Calculate(int multiplierAmount)
+    {
+        int tempValue = this->value;
+        tempValue *= multiplierAmount;
+        return tempValue;
+    }
+
+    void Draw(Color color)
+    {
+        if (shape == 1)
+        {
+            DrawCircle(position.x,position.y, radius, color);
+            if (value == 200)
+            {
+                DrawCircle(position.x, position.y, radius * .66, GOLD);
+            }
+        }
+        else if (shape ==2)
+        {
+            DrawRectangle(rect.x, rect.y, rect.width, rect.height, color); 
+        }
+    }
+
+    Vector3 GetSize() const
+    {
+        Vector3 size = { rect.width,rect.height,radius };
+        return size;
+    }
+
+    float GetXPos() const
+    {
+        return position.x;
+    }
+    float GetYPos() const
+    {
+        return position.y;
+    }
+  
+
+    int GetValue()
+    {
+        return value;
+    }
+
+    void SetPosition(float xPos, float yPos)
+    {
+        position.x = xPos;
+        position.y = yPos;
+        rect.x = position.x;
+        rect.y = position.y;
+    }
+
+
+    void SetRotation(float rotation)
+    {
+        this->rotation = rotation;
+    }
+
+private:
+    Vector2 position;
+    Rectangle rect;
+    float radius;
+    int value;
+    int shape;
+    int rotation;
+};
+
+
+
 int main(void)
 {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
+
+
     float buttonRadius = 50;
-    Rectangle backScreen = Rectangle{0,0,SCREEN_WIDTH,SCREEN_HEIGHT };
+
+    Rectangle backScreen = Rectangle{ 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };
     Rectangle readScreen = { backScreen.width * .025,backScreen.height * .11,backScreen.width * .95,130 };
-    map<string, Vector2> button;
-    button["one"] = {static_cast<float>(backScreen.width * .12),static_cast<float>(backScreen.height * .85)};
-    button["two"] = {button["one"].x+(buttonRadius*2)+33,button["one"].y};
-    button["three"] = {button["two"].x+(buttonRadius*2)+33,button["two"].y};
-    button["four"] = {button["one"].x,button["one"].y - (buttonRadius*2)-25};
-    button["five"] = {button["two"].x,button["four"].y};
-    button["six"] = {button["three"].x,button["four"].y};
-    button["seven"] = {button["four"].x,button["four"].y-(buttonRadius*2)-25};
-    button["eight"] = {button["five"].x,button["seven"].y};
-    button["nine"] = {button["six"].x,button["eight"].y};
-    button["zero"] = {0,0};
-    button["equal"] = {0,0};
-    button["minus"] = {0,0};
-    button["plus"] = {0,0};
-    button["multiply"] = {0,0};
-    button["divide"] = {0,0};
-    button["clear"] = {0,0};
+    map<string, Vector3> button;
+
+                    
+    button["QUARTER"] = { 25,100,25 };
+    button["LOONIE"] = { 10,0,0 };
+    button["TOONIE"] = { 20,0,0 };
+    button["FIVE"] = { 50,0,0 };
+    button["TEN"] = { 100,0,0 };
+    button["TWENTY"] = { 200,0,0 };
+    button["FIFTY"] = { 500,0,0 };
+    button["HUNDRED"] = { 1000,0,0 };
+
+
+    button["nine"] = {};
+    button["zero"] = {};
+    button["equal"] = { 0,0 };
+    button["minus"] = { 0,0 };
+    button["plus"] = { 0,0 };
+    button["multiply"] = { 0,0 };
+    button["divide"] = { 0,0 };
+    button["clear"] = { 0,0 };
     button["point"] = { 0,0 };
 
+    currencyButton quarter(25, { 50,50,0,0 }, buttonRadius,1);
+    currencyButton loonie(100, { 50,50,0,0 }, buttonRadius,1);
+    currencyButton toonie(200, { 50,50,0,0 }, buttonRadius,1);
+    currencyButton five(500, { 50,50,152,70 },0,2);
+    currencyButton ten(1000, { 50,50,152,70 }, 0,2);
+    currencyButton twenty(2000, { 50,50,152,70 }, 0,2);
+    currencyButton fifty(5000, { 50,50,152,70 }, 0, 2);
+    currencyButton hundred(10000, { 50,50,152,70 }, 0, 2);
 
-    {   /*
-        std::map<string, Rectangle> boxes;
-        boxes["box1"] = { -500,-500,40,40 };
-        boxes["box2"] = { -500,-500,150,200 };
-        boxes["box3"] = { -500,-500,20,60 };
-        boxes["box4"] = { -500,-500,80,50 };
-        boxes["box5"] = { -500,-500,250,15 };
-        //*/
-    }
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
+
+
+ 
     SetTargetFPS(60);
 
     int rotation;
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(RAYWHITE);
         //DrawText("Hello World!", 16, 9, 20, RED);
+
+        quarter.SetPosition(SCREEN_WIDTH * .2, SCREEN_HEIGHT * .17);
+        loonie.SetPosition(quarter.GetXPos() + (loonie.GetSize().z * 2)+15, quarter.GetYPos());
+        toonie.SetPosition(loonie.GetXPos() + (toonie.GetSize().z * 2) + 15, loonie.GetYPos());
+        five.SetPosition(quarter.GetXPos()-(quarter.GetSize().z), quarter.GetYPos() + (five.GetSize().y*2));
+        ten.SetPosition(five.GetXPos(), five.GetYPos() + (five.GetSize().y+15));
+        twenty.SetPosition(ten.GetXPos(), ten.GetYPos() + (ten.GetSize().y +15));
+        fifty.SetPosition(twenty.GetXPos(), twenty.GetYPos() + (twenty.GetSize().y + 15));
+        hundred.SetPosition(fifty.GetXPos(), fifty.GetYPos() + (fifty.GetSize().y + 15));
+
 
         //Set Mouse Position x and y to new Var
         mousePOS = GetMousePosition();
         //Set which key is press to new Var
         int key = GetKeyPressed();
-      
-        //Adding Rectangles
-        {   /*
-            switch (key)
-            {
-            case KEY_ONE:
-                boxes["box1"].x = mousePOS.x - (boxes["box1"].width / 2);
-                boxes["box1"].y = mousePOS.y - (boxes["box1"].height / 2);
-                key = NULL;
-                break;
-            case KEY_TWO:
-                boxes["box2"].x = mousePOS.x - (boxes["box2"].width / 2);
-                boxes["box2"].y = mousePOS.y - (boxes["box2"].height / 2);
-                key = NULL;
-                break;
-            case KEY_THREE:
-                boxes["box3"].x = mousePOS.x - (boxes["box3"].width / 2);
-                boxes["box3"].y = mousePOS.y - (boxes["box3"].height / 2);
-                key = NULL;
-                break;
-            case KEY_FOUR:
-                boxes["box4"].x = mousePOS.x - (boxes["box4"].width / 2);
-                boxes["box4"].y = mousePOS.y - (boxes["box4"].height / 2);
-                key = NULL;
-                break;
-            case KEY_FIVE:
-                boxes["box5"].x = mousePOS.x - (boxes["box5"].width / 2);
-                boxes["box5"].y = mousePOS.y - (boxes["box5"].height / 2);
-                key = NULL;
-                break;
-            case KEY_BACKSPACE:
-                for (auto& element : boxes)
-                {
-                    element.second.x = -500;
-                }
-
-                break;
-            }
-            //Drawing Rectangles
-            DrawRectanglePro(boxes["box1"], { 0,0 }, 0, GREEN);
-            DrawRectanglePro(boxes["box2"], { 0,0 }, 0, RED);
-            DrawRectanglePro(boxes["box3"], { 0,0 }, 0, BLUE);
-            DrawRectanglePro(boxes["box4"], { 0,0 }, 0, PURPLE);
-            DrawRectanglePro(boxes["box5"], { 0,0 }, 0, ORANGE);
-            //*/
-        }
-        
-        DrawRectangleRounded(backScreen, .25, 0, { 42, 47, 79,255 });
-        DrawRectangleRounded(readScreen, .10, 0, { 145, 127, 179,200 });
-        DrawCircleV(button["one"], buttonRadius, {229, 190, 236,255});
-        DrawCircleV(button["two"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["three"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["four"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["five"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["six"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["seven"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["eight"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["nine"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["zero"], buttonRadius, { 229, 190, 236,255 });
-        DrawCircleV(button["clear"], buttonRadius, { 229, 190, 236,255 });
 
 
+        quarter.Draw(LIGHTGRAY);
+        loonie.Draw(BROWN);
+        toonie.Draw(LIGHTGRAY);
+        five.Draw(BLUE);
+        ten.Draw(PURPLE);
+        twenty.Draw(GREEN);
+        fifty.Draw(RED);
+        hundred.Draw(BEIGE);
 
 
-         EndDrawing();
+        EndDrawing();
     }
 
     CloseWindow();
