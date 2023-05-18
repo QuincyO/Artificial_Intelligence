309ee76a41af658057e8060c7cc8a6d3a7d5d954
QuincyO <quincyorta@gmail.com>
2023-05-18 13:03:58 -0400

    Added Position, Velocity, Accleration sliders

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,6 +3,15 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+Vector2 WrapAroundScreen(Vector2 position)
+{
+    Vector2 outposition =
+    {
+        (int)(position.x + SCREEN_WIDTH) % SCREEN_WIDTH,
+        (int)(position.y + SCREEN_HEIGHT)%SCREEN_HEIGHT,
+    };
+    return outposition;
+}
 
 int main(void)
 {
@@ -10,9 +19,11 @@ int main(void)
     rlImGuiSetup(true);
     SetTargetFPS(240);
 
-    Vector2 position = { 100,100 };
-    Vector2 velocity = { 10,0 };
+    Vector2 position = { 100,100 };//in px
+    Vector2 velocity = { 10,0 }; //In px/s
     float maxSpeed = 1000;
+    float maxAccel = 50;
+    Vector2 acceleration = { 0,50 }; //In px/s/s
 
     //Need three different parameters:
     /*
@@ -34,24 +45,27 @@ int main(void)
         ClearBackground(RAYWHITE);
 
 
-
         const float dt = GetFrameTime();
-
+        Vector2 V2 = { 25,0 };
             rlImGuiBegin();
 
             ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
             ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
+            ImGui::SliderFloat2("Acceleration", &(acceleration.x), -maxAccel, maxAccel);
 
 
 
             rlImGuiEnd();
 
             Vector2 displacement;
-            displacement = velocity * dt;
+            displacement = velocity * dt; // px/s * s = px
 
-            position = position + displacement;
+            position = position + displacement + acceleration + 0.5f * dt * dt;
 
+            Vector2 deltaV = acceleration * dt; // px/s/s * s = px/s
 
+            velocity = deltaV + velocity;
+            position = WrapAroundScreen(position);
             DrawCircleV(position, 45, RED);
 
         EndDrawing();
