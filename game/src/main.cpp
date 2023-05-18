e2701aa9c088b2f30d5cbad55a932fa13f9fe55e
QuincyO <quincyorta@gmail.com>
2023-05-18 12:17:11 -0400

    lab 2

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,17 +1,59 @@
 #include "rlImGui.h"
+#include "Math.h"
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+
 
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    SetTargetFPS(60);
+    rlImGuiSetup(true);
+    SetTargetFPS(240);
+
+    Vector2 position = { 100,100 };
+    Vector2 velocity = { 10,0 };
+    float maxSpeed = 1000;
+
+    //Need three different parameters:
+    /*
+        Position-Location on screen, expressed as a displacement from some reference point (0,0).
+            In raylib, its the top left, and we an use Pixels (px) as our unit for this.
+
+        Velocity - Change in position over time. We can use px/sec as our unit for this
+
+        Acceleration - Changes in velocity over time. We an use px/sec/sec as our unit for this.
+
+
+    
+    */
+
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+
+
+
+        const float dt = GetFrameTime();
+
+            rlImGuiBegin();
+
+            ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
+            ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
+
+
+
+            rlImGuiEnd();
+
+            Vector2 displacement;
+            displacement = velocity * dt;
+
+            position = position + displacement;
+
+
+            DrawCircleV(position, 45, RED);
+
         EndDrawing();
     }
 
