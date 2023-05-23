b79819d0aa543f61d25ef7433177b87b030ee1e8
QuincyO <quincyorta@gmail.com>
2023-05-22 20:24:35 -0400

    Remote Push

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,23 +7,42 @@ Vector2 WrapAroundScreen(Vector2 position)
 {
     Vector2 outposition =
     {
-        (int)(position.x + SCREEN_WIDTH) % SCREEN_WIDTH,
+       (int) (position.x + SCREEN_WIDTH) % SCREEN_WIDTH,
         (int)(position.y + SCREEN_HEIGHT)%SCREEN_HEIGHT,
     };
     return outposition;
 }
 
+Vector2 Floor(Vector2 position,Vector2& velocity)
+{
+    Vector2 FloorV =
+    {
+        position.x,
+        position.y,
+    };
+
+    if (FloorV.y > SCREEN_HEIGHT - 45)
+    {
+        FloorV.y = SCREEN_HEIGHT - 45;
+        velocity.y = 0;
+    }
+    return FloorV;
+}
+
+
+
+
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     rlImGuiSetup(true);
-    SetTargetFPS(240);
+    SetTargetFPS(60);
 
-    Vector2 position = { 100,100 };//in px
-    Vector2 velocity = { 10,0 }; //In px/s
+    Vector2 position = { 500,0 };//in px
+    Vector2 velocity = { 0,0 }; //In px/s
     float maxSpeed = 1000;
-    float maxAccel = 50;
-    Vector2 acceleration = { 0,50 }; //In px/s/s
+    float maxAccel = 250;
+    Vector2 acceleration = { 0,25 }; //In px/s/s
 
     //Need three different parameters:
     /*
@@ -38,39 +57,50 @@ int main(void)
     
     */
 
-
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
+            rlImGuiBegin();
 
         const float dt = GetFrameTime();
-        Vector2 V2 = { 25,0 };
-            rlImGuiBegin();
+    
+
+         //   ImGui::SliderFloat2("Acceleration", &(acceleration.x), -maxAccel, maxAccel);
+
+
+
+
+            Vector2 displacement;
+
+            displacement = velocity * dt; //In px/s * s, which cancels out the seconds and is just Px. So the displacement becomes the position on screen.
+
+
+            velocity = velocity + acceleration * dt;
+
+            //position is where the object is going to be after all the relevent information is calculated
+
+
+            Vector2 deltaV = acceleration * dt; // px/s/s * s = px/s
+            position = position + velocity;
+
+            //velocity = velocity + deltaV;
+
+
+
+            position = Floor(position,velocity);
 
             ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
             ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
-            ImGui::SliderFloat2("Acceleration", &(acceleration.x), -maxAccel, maxAccel);
 
-
-
-            rlImGuiEnd();
-
-            Vector2 displacement;
-            displacement = velocity * dt; // px/s * s = px
-
-            position = position + displacement + acceleration + 0.5f * dt * dt;
-
-            Vector2 deltaV = acceleration * dt; // px/s/s * s = px/s
-
-            velocity = deltaV + velocity;
-            position = WrapAroundScreen(position);
+            DrawCircleV(GetMousePosition(), 30, BLACK);
             DrawCircleV(position, 45, RED);
 
+        rlImGuiEnd();
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
+
 }
