dff4fb310963c44ba837373bafccc490aa8a889b
QuincyO <quincyorta@gmail.com>
2023-05-22 22:45:39 -0400

    WOOOOHOOO WE DID IT kinda...

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,5 +1,6 @@
 #include "rlImGui.h"
 #include "Math.h"
+#include <iostream>
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
@@ -27,6 +28,16 @@ public:
         return m_velocity;
     }
 
+    void SetVelocity(Vector2 Velo)
+    {
+        m_velocity = Velo;
+    }
+
+    void SetPosition(Vector2 position)
+    {
+        m_position = position;
+    }
+
 private:
     Vector2 m_position;
     Vector2 m_velocity;
@@ -34,6 +45,40 @@ private:
     Vector2 m_displacement;
 
 };
+
+class Agent
+{
+public:
+    Agent(Vector2 position, Vector2 accel, Vector2 velo)
+    {
+        m_fish = new RigidBody(position, accel, velo);
+
+    }
+
+    ~Agent()
+    {
+        delete m_fish;
+        m_fish = nullptr;
+    }
+
+    void Update(float deltaTime)
+    {
+        m_fish->GetPosition();
+    }
+
+
+private:
+    float m_maxSpeed = 350; // 350 Px/s 
+    float m_maxAacceleration; // 50 Px/s /s
+
+    RigidBody* m_fish;
+
+};
+
+
+
+
+
 
 Vector2 WrapAroundScreen(Vector2 position)
 {
@@ -70,10 +115,12 @@ int main(void)
     rlImGuiSetup(true);
     SetTargetFPS(60);
 
-    Vector2 position = { 500,0 };//in px
+    float timer = 0;
+
+    Vector2 position = { 500,250 };//in px
     Vector2 velocity = { 0,0 }; //In px/s
-    float maxSpeed = 1000;
-    float maxAccel = 250;
+    float maxSpeed = 25;
+    float maxAccel = 100;
     Vector2 acceleration = { 0,25 }; //In px/s/s
 
     //Need three different parameters:
@@ -98,34 +145,48 @@ int main(void)
         const float dt = GetFrameTime();
     
 
-         //   ImGui::SliderFloat2("Acceleration", &(acceleration.x), -maxAccel, maxAccel);
-
+        Vector2 circle2Pos = { 750,250 };
 
 
 
             Vector2 displacement;
 
             displacement = velocity * dt; //In px/s * s, which cancels out the seconds and is just Px. So the displacement becomes the position on screen.
-
-
-            velocity = velocity + acceleration * dt;
+            //velocity = velocity + acceleration * dt;
 
             //position is where the object is going to be after all the relevent information is calculated
-
-
             Vector2 deltaV = acceleration * dt; // px/s/s * s = px/s
+          //  position = position + velocity;
+
+            
+            //velocity = velocity + deltaV;
+         //   position = Floor(position,velocity);
+
+
+
+
+
+            Vector2 posToAgent =
+            {
+                GetMousePosition().x - position.x,
+                GetMousePosition().y - position.y
+            };
+
+            posToAgent = Normalize(posToAgent);
+            
+            Vector2 desiredVel = posToAgent * maxSpeed;
+
+            Vector2 deltaVel = desiredVel - velocity;
+
+            Vector2 DeltaAccel = Normalize(deltaVel) * maxAccel;
+
+            velocity = velocity + DeltaAccel * dt;
+
             position = position + velocity;
 
-            //velocity = velocity + deltaV;
+            
 
-
-
-            position = Floor(position,velocity);
-
-            ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
-            ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
-
-            DrawCircleV(GetMousePosition(), 30, BLACK);
+            DrawCircleV(circle2Pos, 30, BLACK);
             DrawCircleV(position, 45, RED);
 
         rlImGuiEnd();
